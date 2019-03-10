d725d95df4648ad10b9774987592341e08dc30c9
SorcererXW <tujipqy@gmail.com>
2019-03-10 10:58:37 +0800

    create symbolic link for tgfont

--- init_script.ps1
+++ init_script.ps1
@@ -1,2 +1,7 @@
+Set-Location ~
+mkdir repo
+
 # create symbolic link for powershell profile
 sudo New-Item -Path $profile -Value ~/repo/dotfiles/ps_profile.ps1 -ItemType SymbolicLink 
+# create symbolic link for tgfont
+sudo New-Item -Path "~\AppData\Roaming\Telegram Desktop\TGFont.json" -Value ~/repo/dotfiles/TGFont.json -ItemType SymbolicLink
