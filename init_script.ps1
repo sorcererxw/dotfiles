dc6a463a45430a4edaabd6923fb4bb6fc364b342
SorcererXW <tujipqy@gmail.com>
2019-03-10 11:23:27 +0800

    update init script

--- init_script.ps1
+++ init_script.ps1
@@ -1,5 +1,22 @@
-Set-Location ~
-mkdir repo
+# init scoop
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install git openssh aria2 concfg sudo
+scoop bucket add extras java
+scoop update
+scoop install shadowsocksr-csharp fiddler typora terminus
+scoop install oraclejdk8
+
+# init ssh
+# ssh-keygen -t rsa -b 4096 -C "tujipqy@gmail.com"
+
+# init git
+git config --global http.proxy 'socks5://127.0.0.1:1080'
+git config --global https.proxy 'socks5://127.0.0.1:1080'
+git config --global user.name "SorcererXW"
+git config --global user.email "tujipqy@gmail.com"
+
+mkdir ~/repo
+git clone git@github.com:sorcererxw/dotfiles.git ~/repo
 
 # create symbolic link for powershell profile
 sudo New-Item -Path $profile -Value ~/repo/dotfiles/ps_profile.ps1 -ItemType SymbolicLink 
