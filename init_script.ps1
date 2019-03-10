dd91fb40ccb1b71d5579eeeac8892a149ef28ade
sorcererxw <tujipqy@gmail.com>
2019-03-10 15:42:27 +0800

    update tgfont

--- init_script.ps1
+++ init_script.ps1
@@ -19,6 +19,6 @@ mkdir ~/repo
 git clone git@github.com:sorcererxw/dotfiles.git ~/repo
 
 # create symbolic link for powershell profile
-sudo New-Item -Path $profile -Value ~/repo/dotfiles/ps_profile.ps1 -ItemType SymbolicLink 
+sudo New-Item -Path $profile -Value ~/repo/dotfiles/ps_profile.ps1 -ItemType SymbolicLink -Force
 # create symbolic link for tgfont
-sudo New-Item -Path "~\AppData\Roaming\Telegram Desktop\TGFont.json" -Value ~/repo/dotfiles/TGFont.json -ItemType SymbolicLink
+sudo New-Item -Path "~\AppData\Roaming\Telegram Desktop\TGFont.json" -Value ~/repo/dotfiles/TGFont.json -ItemType SymbolicLink -Force
